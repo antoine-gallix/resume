7c75556d880b443f9100c3e4bd6ca7bb0e6854c5
Antoine Gallix <7696611+antoine-gallix@users.noreply.github.com>
2025-11-03 03:39:58 +0100

    custom code for autobuild

--- tasks.py
+++ tasks.py
@@ -1,9 +1,12 @@
 import json
-import pathlib
+import shutil
+import time
 import tomllib
+from dataclasses import dataclass
 from functools import reduce
 from operator import or_
 from pathlib import Path
+from typing import Callable
 
 from invoke import task  # type:ignore
 from jinja2 import Environment, FileSystemLoader
@@ -11,17 +14,57 @@ from rich import print
 
 from config import settings
 
-# ------------------------ config ------------------------
-
+# ---
 
 TEMPLATE_DIR = "templates"
 HTML_NAME = "resume.html"
 STATIC_DIR = "static"
-SERVER_SOCKET = "localhost:35729"
+SERVER_HOST = "localhost"
+SERVER_PORT = 35729
+
+# ---
 
 
-def resolved_path(path: str) -> Path:
+def resolved_path(path: str):
     return Path(path).expanduser()
+
+
+STATIC_DIR = resolved_path(STATIC_DIR)
+TEMPLATE_DIR = resolved_path(TEMPLATE_DIR)
+DATA_DIR = resolved_path(settings.DATA_DIR)
+BUILD_DIR = resolved_path(settings.BUILD_DIR)
+URL = f"{SERVER_HOST}:{SERVER_PORT}/{HTML_NAME}"
+
+# ---
+
+
+@dataclass
+class Watcher:
+    targets: list[Path]
+    callback: Callable
+
+    def files(self):
+        for target in self.targets:
+            for dir_, _, files in target.walk():
+                for file in files:
+                    yield dir_ / file
+
+    def mtimes(self):
+        return [(str(file), file.stat().st_mtime) for file in self.files()]
+
+    def watch(self):
+        last_times = self.mtimes()
+        try:
+            while True:
+                time.sleep(1)
+                new_times = self.mtimes()
+                if new_times == last_times:
+                    pass
+                else:
+                    last_times = new_times
+                    self.callback()
+        except KeyboardInterrupt:
+            exit()
 
 
 def read_data(path: Path, first_call=True) -> dict:
@@ -43,51 +86,63 @@ def read_data(path: Path, first_call=True) -> dict:
 
 
 def write(content, name):
-    output_file = resolved_path(settings.BUILD_DIR) / name
+    output_file = BUILD_DIR / name
     output_file.write_text(content)
     print(f"file written: {output_file}")
 
 
+# ---
+
+
 @task
 def show_data(context):
-    data = read_data(resolved_path(settings.DATA_DIR))
+    data = read_data(DATA_DIR)
     json_data = json.dumps(data, indent=4)
     print(json_data)
 
 
-@task
-def build(context):
-    template_dir = resolved_path(TEMPLATE_DIR)
-    build_dir = pathlib.Path(settings.BUILD_DIR).expanduser()
-
-    build_dir.mkdir(exist_ok=True, parents=True)
+def _build():
+    BUILD_DIR.mkdir(exist_ok=True, parents=True)
 
     print("build resume from templates")
 
     env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
-    data = read_data(Path(settings.DATA_DIR).expanduser())
+    data = read_data(DATA_DIR)
 
     html_template = env.get_template(f"{HTML_NAME}.jinja")
     html_output = html_template.render(**data)
     write(html_output, HTML_NAME)
 
-    for file in resolved_path(STATIC_DIR).iterdir():
-        context.run(f"cp {file} {build_dir / file.name}", echo=True)
+    for file in STATIC_DIR.iterdir():
+        shutil.copy(file, BUILD_DIR)
 
     print("resume generated successfully")
 
 
 @task
+def build(context):
+    _build()
+
+
+@task
 def autobuild(context):
-    context.run(f"fd . {TEMPLATE_DIR} | entr invoke build", echo=True)
+    watcher = Watcher(targets=[STATIC_DIR, TEMPLATE_DIR], callback=_build)
+    watcher.watch()
 
 
 @task
 def serve(context):
-    print(f"output served at {SERVER_SOCKET}/{HTML_NAME}")
-    context.run(f"livereload {settings.BUILD_DIR}", echo=True)
+    print(f"output served at {URL}")
+    from livereload import Server
+
+    server = Server()
+    # prevent caching
+    server.setHeader("Cache-Control", "no-store")
+    for ext in ["html", "css", "png"]:
+        server.watch(str(BUILD_DIR / f"*.{ext}"))
+    server.serve(root=BUILD_DIR, port=SERVER_PORT, host=SERVER_HOST)
 
 
 @task
 def firefox(context):
-    context.run(f"firefox --new-window {SERVER_SOCKET}/{HTML_NAME}")
+    context.run(f"firefox --new-window {URL}")
